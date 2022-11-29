5317dea88ab4d74a9e3a640108c9bc9b47176f7b
Ítalo Gonçalves <italogmfaria@gmail.com>
2022-11-28 21:06:24 -0300

    echo "# whokilled" >> README.md git init git add README.md git commit -m "VERSÃO 1.1" git branch -M main git remote add origin https://github.com/italogmfaria/whokilled.git git push -u origin main

--- Who_killed/frontend/Lobby.java
+++ Who_killed/frontend/Lobby.java
@@ -1,4 +1,19 @@
 package Who_killed.frontend;
 
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
 public class Lobby {
+    private JPanel panel1;
+    private JLabel label1;
+    private JButton playButton;
+
+    public Lobby() {
+        panel1.addComponentListener(new ComponentAdapter() {
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
