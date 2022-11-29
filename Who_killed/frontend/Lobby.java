e4f3665b7487637ff6512ba9ab4ce5eccd24cea0
Ítalo Gonçalves <italogmfaria@gmail.com>
2022-11-29 07:29:22 -0300

    echo "# whokilled" >> README.md git init git add README.md git commit -m "VERSÃO 1.1" git branch -M main git remote add origin https://github.com/italogmfaria/whokilled.git git push -u origin main

--- Who_killed/frontend/Lobby.java
+++ Who_killed/frontend/Lobby.java
@@ -1,3 +1,13 @@
+/*
+echo "# whokilled" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/italogmfaria/whokilled.git
+git push -u origin main
+ */
+
 package Who_killed.frontend;
 
 import javax.swing.*;
