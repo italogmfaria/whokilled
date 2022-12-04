a9e66d69c237b3f32669bea79767581f2299675b
José Antonio Ribeiro Souto <josejarsbr@gmail.com>
2022-12-04 11:20:03 -0300

    Versao 1.7
    Adição sistema de pontuação, e imprimir regras e organização geral

--- Who_killed/Investigacao.java
+++ Who_killed/Investigacao.java
@@ -20,11 +20,12 @@ public class Investigacao {
     public static final String PURPLE = "\u001B[35m";
     public static final String GREEN = "\u001B[32m";
     public static final String RED = "\u001B[31m";
-    public static String CULPADO, CAMINHO, RESUMO1, RESUMO2, DIALOGO01, DIALOGO02, DIALOGO03;
+    public static String CULPADO, CAMINHO, RESUMO1, RESUMO2, DIALOGO01, DIALOGO02, DIALOGO03, NOME, NICK;
     public static String[] INOCENTES = new String[3];
-    public static String ESPACO = "\n------------------------------------------\n";
+    public static String ESPACO = "\n-------------------------------------------\n";
     public static boolean SUCESSO = true;
     public static int PONTOS = 0, CONTROLADOR = 1;
+    public static int[] PONTOCADACASO = new int[7];
 
     public static Scanner ler = new Scanner(System.in);
 
@@ -32,10 +33,33 @@ public class Investigacao {
     public static File arquivo = new File("Who_killed/jogador.txt");
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
+        regras();
         registraJogador(); //Esse e o metodo que vai pegar o nome de o nick do jogador e salvar.
         jogo();//Esse metodo e o corpo do jogo
     }
+
+    public static void regras() {
+        System.out.println(ESPACO);
+        System.out.print("""
+                Bem Vindo ao Who Killed, uma simulação de investigação
+                As regras são simples
+                ACERTO O QUEM E O ASSASINO
+                BLABLALBALBABALB
+                BLABLBABLABBALB
+                BLABABALB
+                                
+                Boa Sorte!!!
+                """);
+        System.out.print(ESPACO);
+    }
+
+    public static void registraJogador() throws IOException {
+        System.out.print("Insira o seu nome: ");
+        NOME = ler.nextLine();
+        System.out.print("Insira um nickname: ");
+        NICK = ler.nextLine();
+    }
+
 
     public static void jogo() throws IOException, InterruptedException {
         CAMINHO = "Who_killed/Crimes/Crime0" + CONTROLADOR + ".txt";
@@ -44,23 +68,6 @@ public class Investigacao {
         vereditoDoCaso(); //Esse e o metodo que vai fazer o chute de quem e o assasino
         epilogo(); //Esse e o metodo final que vai falar se voce acertou ou nao e imprimir o resumo e salva a pontuação
 
-    }
-
-    public static void registraJogador() throws IOException {
-        System.out.print("Nome: ");
-        String nome = ler.nextLine();
-        System.out.print("Apelido: ");
-        String apelido = ler.nextLine();
-        writeFile(arquivo, nome, apelido);
-    }
-
-    public static void writeFile(File arquivo, String nome, String apelido) throws IOException {
-        FileWriter fw = new FileWriter(arquivo, true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(String.format("nome = %s : apelido = %s", nome, apelido));
-        bw.newLine();
-        bw.close();
-        fw.close();
     }
 
     public static void pistasDoCrime() throws FileNotFoundException {
@@ -92,10 +99,11 @@ public class Investigacao {
             if (resposta.equalsIgnoreCase("sim")) {
                 System.out.println(BLUE + DIALOGO01 +
                         "\n" + DIALOGO02 + RESET);
-
+                PONTOCADACASO[CONTROLADOR] = 0;
                 return;
-            } else if (resposta.equalsIgnoreCase("nao")) {
+            } else if (resposta.equalsIgnoreCase("nao") || resposta.equalsIgnoreCase("não")) {
                 System.out.println("-Acho melhor nao mecher nas coisas alheias");
+                PONTOCADACASO[CONTROLADOR] = 2;
                 return;
             } else {
                 System.out.println("-Será que devo verificar as gravacoes? 'sim' ou 'nao'");
@@ -107,9 +115,10 @@ public class Investigacao {
         System.out.println(ESPACO);
         //System.out.println(RESUMO1); RESUMO DENCESSASARIO
         //Thread.sleep(2000);
-        System.out.println("-Ao analizar a ficha por algum tempo você sente que está pronto para identificar o culpado," +
-                "certo, consegui provas o suficente para indentificar o assasino." +
-                "\nO culpado esta entre essas pessoas\n");
+        System.out.println("""
+                -Ao analizar a ficha por algum tempo você sente que está pronto para identificar o culpado,certo, consegui provas o suficente para indentificar o assasino.
+                O culpado esta entre essas pessoas
+                """);
 
         System.out.println("=================");
         int random = gerador.nextInt(3) + 1;
@@ -149,20 +158,47 @@ public class Investigacao {
         System.out.println(ESPACO);
         if (SUCESSO) {
             System.out.println("Seu papite estava " + GREEN + "CERTO" + RESET);
-            PONTOS++;
-            System.out.println("Pontuação --- " + PONTOS);
-            CONTROLADOR++;
             System.out.println(BLUE + RESUMO2 + RESET);
+            System.out.println("Pontuação --- " + PONTOS);
+            PONTOCADACASO[CONTROLADOR] += 3;
+            CONTROLADOR++;
             if (CONTROLADOR < 7) {
                 jogo();
+            } else {
+                CONTROLADOR--;
+                System.out.println("--> Parabens Você Acertou Todos os Casos <--");
+                imprimirResumoFinal();
             }
         } else {
             System.out.println("Seu papite estava " + RED + "ERRADO" + RESET);
             System.out.println(BLUE + RESUMO2 + RESET);
+            PONTOCADACASO[CONTROLADOR] = 0;
+            imprimirResumoFinal();
         }
-        System.out.println("Sua pontuação final é " + PONTOS);
-        System.out.println(ESPACO);
     }
 
+    public static void imprimirResumoFinal() throws IOException {
+        System.out.println(ESPACO);
+        System.out.println("\t========== Resumo final ==========");
+        System.out.println("\t Jogador ---> " + NOME);
+        System.out.println("\t Nick  --->  " + NICK);
+        System.out.println("\t Pontuação:");
+        for (int i = 1; i <= CONTROLADOR; i++) {
 
+            System.out.println("\t\t Caso " + i + "º ---> " + PONTOCADACASO[i] + " Pontos");
+            PONTOS += PONTOCADACASO[i];
+        }
+        System.out.println("\t Pontuação Final ---> " + PONTOS);
+        System.out.println(ESPACO);
+        writeFile();
+    }
+
+    public static void writeFile() throws IOException {
+        FileWriter fw = new FileWriter(arquivo, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(String.format("*Nome = %s \t*Nick = %s \t *Pontuação = %d", NOME, NICK, PONTOS));
+        bw.newLine();
+        bw.close();
+        fw.close();
+    }
 }
